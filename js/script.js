c51aeee0961069825acad6778a97eefcf3c7613b
Maciek Bulski <fragmast3r@gmail.com>
2019-02-23 13:42:39 +0000

    add typing effect

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function typeEffect(element, speed) {
+	var text = $(element).text();
+	$(element).html('');
+	
+	var i = 0;
+	var timer = setInterval(function() {
+					if (i < text.length) {
+						$(element).append(text.charAt(i));
+						i++;
+					} else {
+						clearInterval(timer);
+					}
+				}, speed);
+}
+
+$( document ).ready(function() {
+  var speed = 75;
+  var delay = $('p').text().length * speed + speed;
+  typeEffect($('p'), speed);
+  
+});
